c4803787b74d086605d4fe4ccc18781b6afbb3d0
sanjidarimi <tumithasan1@gmail.com>
2026-07-16 21:44:13 +0600

    chnage ts config

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,6 +1,6 @@
 import { PrismaPg } from "@prisma/adapter-pg";
 import "dotenv/config";
-import { PrismaClient } from "../../generated/prisma/client";
+import { PrismaClient } from "../../generated/prisma/client.js";
 
 const connectionString = `${process.env.DATABASE_URL}`;
 
